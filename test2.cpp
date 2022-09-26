971316748e9d44e251da56ffe5941b0a393533bc
GaoHuic <102906462+GaoHuic@users.noreply.github.com>
2022-09-26 21:48:22 +0800

    Create test2.cpp

---
+++ test2.cpp
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+int main()
+{
+
+  while(1)
+  {
+    printf("To always adhere to you own. Be confident to face your own future.");
+    printf("Don't worry about others in any time. Stick to yourself. Make a plan and do it every day");
+  }
+  
+  retuen 0;
+}
