b0aab42f2e919c41648affd9d2af8279bc941527
GaoHuic <102906462+GaoHuic@users.noreply.github.com>
2022-09-26 21:42:34 +0800

    Create test1.cpp

---
+++ test1.cpp
@@ -0,0 +1,11 @@
+#include “stdio.h”
+
+int mian()
+{
+ 
+  printf("Gao Hui cheng is one of the greast persons in the world");
+  printf("His ambition is to change the world, because of him.");
+  
+  return 0;
+
+}
